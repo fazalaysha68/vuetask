4f7149ac3d758f842203149ef77588dbce273bb0
fazalaysha68 <73339826+fazalaysha68@users.noreply.github.com>
2021-01-12 10:29:10 +0500

    Initial commit

---
+++ resources/js/components/About.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-2xl text-gray-800">
+       About Page
+    </div>
+</template>
---
+++ resources/js/components/Register.vue
@@ -0,0 +1,76 @@
+<template>
+      <div class="flex flex-wrap w-full justify-center items-center pt-56">
+        <div class="flex flex-wrap max-w-xl">
+            <div class="p-2 text-2xl text-gray-800 font-semibold"><h1>Register an account</h1></div>
+            <div class="p-2 w-full">
+                <label class="w-full" for="name">Name</label>
+                <span class="w-full text-red-500" v-if="errors.name">{{errors.name[0]}}</span>
+                <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Name" type="text" v-model="form.name" >
+            </div>
+            <div class="p-2 w-full">
+                <label for="email">Your e-mail</label>
+                <span class="w-full text-red-500" v-if="errors.email">{{errors.email}}</span>
+                <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Email" type="email" v-model="form.email">
+            </div>
+            <div class="p-2 w-full">
+                <label for="password">Password</label>
+                <span class="w-full text-red-500" v-if="errors.password">{{errors.password}}</span>
+                <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Password" type="password" v-model="form.password" name="password">
+            </div>
+            <div class="p-2 w-full">
+                <label for="confirm_password">Confirm Password</label>
+                <span class="w-full text-red-500" v-if="errors.password_confirmation">{{errors.password_confirmation}}</span>
+                <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Confirm Password" type="password" v-model="form.password_confirmation" name="password_confirmation">
+            </div>
+            <div class="p-2 w-full">
+                <label>
+                    <input type="radio" class="form-control" name="gender" value="male" v-model="form.gender" />
+                    Male
+                </label>
+                <span class="w-full text-red-500" v-if="errors.gender">{{ errors.gender }}</span>
+            </div>   
+            <div class="p-2 w-full mt-4">
+                <button @click.prevent="saveForm" type="submit" class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
+            </div>
+        </div> 
+    </div>
+</template>
+<script>
+import { required, email, minLength, sameAs } from "vuelidate/lib/validators";
+
+export default {
+    name: "app",
+    data(){
+        return{
+            form:{
+                name: '',
+                email: '',
+                password:'',
+                password_confirmation:'',
+                gender: ''
+                
+            },
+            errors:[]
+        }
+    },
+        validations: {
+            user: {
+                name: { required },
+                email: { required, email },
+                password: { required, minLength: minLength(6) },
+                password_confirmation: { required, sameAsPassword: sameAs('password') },
+                gender: { required },
+                
+            }
+        },
+    methods:{
+        saveForm(){
+            axios.post('/api/register', this.form).then(() =>{
+                console.log('saved');
+            }).catch((error) =>{
+                this.errors = error.response.data.errors;
+            })
+        }
+    }
+}
+</script>
