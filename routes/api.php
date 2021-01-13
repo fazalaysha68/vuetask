487a08038f0357874ee31856067f89eee2304909
fazalaysha68 <73339826+fazalaysha68@users.noreply.github.com>
2021-01-13 07:14:37 +0500

    Commit

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,13 @@ Route::post('register', 'RegisterController@register');
 Route::post('login', 'LoginController@login');
 Route::post('logout', 'LoginController@logout');
 
+Route::prefix('auth')->group(function () {
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+    Route::get('refresh', 'AuthController@refresh');
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::get('user', 'AuthController@user');
+        Route::post('logout', 'AuthController@logout');
+    });
+});
+
